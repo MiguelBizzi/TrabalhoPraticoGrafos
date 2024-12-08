69bdc7525f303c5c6b8650499fb8b7932530d14f
miguelbizzi <miguel.bizzi@aglets.com.br>
2024-12-08 01:20:17 -0300

    adicionando questoes

--- TrabalhoPraticoGrafos/Grafo.cs
+++ TrabalhoPraticoGrafos/Grafo.cs
@@ -16,8 +16,8 @@
 
     public void AdicionarAresta(int saida, int entrada, double peso)
     {
-        var verticeSaida = vertices.Find(v => v.Indice == saida);
-        var verticeEntrada = vertices.Find(v => v.Indice == entrada);
+        Vertice? verticeSaida = vertices.Find(v => v.Indice == saida);
+        Vertice? verticeEntrada = vertices.Find(v => v.Indice == entrada);
 
         if (verticeSaida == null || verticeEntrada == null)
         {
@@ -37,14 +37,14 @@
     public void ImprimirListaAdjacencia()
     {
         Console.WriteLine("==== Lista de Adjacência ====");
-        var adjacencia = new Dictionary<int, List<(int, double)>>();
+        Dictionary<int, List<(int, double)>> adjacencia = new Dictionary<int, List<(int, double)>>();
 
-        foreach (var vertice in vertices)
+        foreach (Vertice vertice in vertices)
         {
             adjacencia[vertice.Indice] = new List<(int, double)>();
         }
 
-        foreach (var aresta in arestas)
+        foreach (Aresta aresta in arestas)
         {
             adjacencia[aresta.VerticeSaida.Indice].Add((aresta.VerticeEntrada.Indice, aresta.Peso));
         }
@@ -78,6 +78,84 @@
                 Console.Write($"{matriz[i, j]} ");
             }
             Console.WriteLine();
+        }
+    }
+
+    public void ImprimirArestasAdjacentes(int origem, int destino)
+    {
+        var aresta = arestas.Find(a => a.VerticeSaida.Indice == origem && a.VerticeEntrada.Indice == destino);
+        if (aresta == null)
+        {
+            Console.WriteLine("Aresta informada não encontrada!");
+            return;
+        }
+
+        Console.WriteLine($"Arestas adjacentes à aresta ({origem} -> {destino}):");
+        foreach (var adj in arestas)
+        {
+            if (adj.VerticeSaida.Indice == aresta.VerticeEntrada.Indice || adj.VerticeEntrada.Indice == aresta.VerticeSaida.Indice)
+            {
+                Console.WriteLine($"({adj.VerticeSaida.Indice} -> {adj.VerticeEntrada.Indice}), peso: {adj.Peso}");
+            }
+        }
+    }
+
+    public void ImprimirVerticesAdjacentes(int vertice)
+    {
+        Console.WriteLine($"Vértices adjacentes ao vértice {vertice}:");
+        foreach (var aresta in arestas)
+        {
+            if (aresta.VerticeSaida.Indice == vertice)
+            {
+                Console.WriteLine(aresta.VerticeEntrada.Indice);
+            }
+        }
+    }
+
+    public void ImprimirArestasIncidentes(int vertice)
+    {
+        Console.WriteLine($"Arestas incidentes no vértice {vertice}:");
+        foreach (var aresta in arestas)
+        {
+            if (aresta.VerticeEntrada.Indice == vertice)
+            {
+                Console.WriteLine($"({aresta.VerticeSaida.Indice} -> {aresta.VerticeEntrada.Indice}), peso: {aresta.Peso}");
+            }
+        }
+    }
+
+    public void ImprimirVerticesIncidentes(int origem, int destino)
+    {
+        var aresta = arestas.Find(a => a.VerticeSaida.Indice == origem && a.VerticeEntrada.Indice == destino);
+        if (aresta == null)
+        {
+            Console.WriteLine("Aresta informada não encontrada!");
+            return;
+        }
+
+        Console.WriteLine($"Vértices incidentes à aresta ({origem} -> {destino}):");
+        Console.WriteLine($"Origem: {aresta.VerticeSaida.Indice}, Destino: {aresta.VerticeEntrada.Indice}");
+    }
+
+    public void ImprimirGrauVertice(int vertice)
+    {
+        int grauEntrada = arestas.Count(a => a.VerticeEntrada.Indice == vertice);
+        int grauSaida = arestas.Count(a => a.VerticeSaida.Indice == vertice);
+        Console.WriteLine($"Grau do vértice {vertice}: Entrada = {grauEntrada}, Saída = {grauSaida}, Total = {grauEntrada + grauSaida}");
+    }
+
+    public void SubstituirPesoAresta(int origem, int destino, double novoPeso)
+    {
+        var aresta = arestas.Find(a => a.VerticeSaida.Indice == origem && a.VerticeEntrada.Indice == destino);
+
+        if (aresta != null)
+        {
+            aresta.Peso = novoPeso;
+            Console.WriteLine($"O peso da aresta ({origem} -> {destino}) foi alterado para {novoPeso}.");
+        }
+        else
+        {
+            Console.WriteLine("Aresta não encontrada!");
         }
     }
 }
--- TrabalhoPraticoGrafos/Program.cs
+++ TrabalhoPraticoGrafos/Program.cs
@@ -50,9 +50,43 @@
         Console.WriteLine();
     }
 
-    static void LerGrafo()
+    static Grafo LerGrafo(string caminhoArquivo)
     {
+        try
+        {
+            string[] linhas = File.ReadAllLines(caminhoArquivo);
 
+            Grafo grafo = new Grafo();
+
+            string[] primeiraLinha = linhas[0].Split(' ');
+            int numeroVertices = int.Parse(primeiraLinha[0]);
+            int numeroArestas = int.Parse(primeiraLinha[1]);
+
+            for (int i = 1; i <= numeroVertices; i++)
+            {
+                grafo.AdicionarVertice(i);
+            }
+
+            for (int i = 1; i < linhas.Length; i++)
+            {
+                var partes = linhas[i].Split(' ');
+                int origem = int.Parse(partes[0]);
+                int destino = int.Parse(partes[1]);
+                double peso = double.Parse(partes[2]);
+
+                grafo.AdicionarAresta(origem, destino, peso);
+            }
+
+            grafo.ImprimirListaAdjacencia();
+
+            return grafo;
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message, "Shape processing failed.");
+            throw;
+        }
     }
 
     static void Main(string[] args)
@@ -61,13 +95,30 @@
         int[,] matrizAdjacencia = null;
         int[,] matrizIncidencia = null;
         Dictionary<int, List<(int, int)>> listaAdjacencia = null;
+        Grafo? grafoLido = null;
 
         while (!encerrarApp)
         {
             Console.WriteLine("==== Menu Principal ====");
             Console.WriteLine("1. Construir Grafo");
             Console.WriteLine("2. Ler grafo");
-            Console.WriteLine("10. Sair");
+            if (grafoLido != null)
+            {
+                Console.WriteLine("3. Imprimir todas as arestas adjacentes de uma aresta");
+                Console.WriteLine("4. Imprimir todas as arestas adjacentes de um vértice");
+                Console.WriteLine("5. Imprimir todas as arestas incidentes de uma aresta");
+                Console.WriteLine("6. Imprimir todas as arestas incidentes de um vértice");
+                Console.WriteLine("7. Imprimir o grau de um vértice");
+                Console.WriteLine("8. Determinar se dois vértices são adjacentes");
+                Console.WriteLine("9. Substituir o peso de uma aresta");
+                Console.WriteLine("10. Trocar dois vértices");
+                Console.WriteLine("11. Busca em Largura");
+                Console.WriteLine("12. Busca em Profundidade");
+                Console.WriteLine("13. Algoritmo de Dijkstra");
+                Console.WriteLine("14. Algoritmo de Floyd Warshall");
+            }
+
+            Console.WriteLine("15. Sair");
             Console.Write("Escolha uma opção: ");
             string opcao = Console.ReadLine();
 
@@ -77,10 +128,77 @@
                     ConstruirGrafo();
                     break;
                 case "2":
-                    LerGrafo();
+                    grafoLido = LerGrafo("../../../grafo.txt");
                     Console.Clear();
                     break;
-                case "10":
+                case "3":
+                    if (grafoLido == null)
+                    {
+                        Console.WriteLine("Grafo não lido!");
+                        break;
+                    }
+
+                    Console.WriteLine("Informe a origem e o destino da aresta (ex: 0 1):");
+                    var entradaAresta = Console.ReadLine().Split(' ');
+                    grafoLido.ImprimirArestasAdjacentes(int.Parse(entradaAresta[0]), int.Parse(entradaAresta[1]));
+                    break;
+                case "4":
+                    if (grafoLido == null)
+                    {
+                        Console.WriteLine("Grafo não lido!");
+                        break;
+                    }
+
+                    Console.WriteLine("Informe o vértice:");
+                    grafoLido.ImprimirVerticesAdjacentes(int.Parse(Console.ReadLine()));
+                    break;
+                case "5":
+                    if (grafoLido == null)
+                    {
+                        Console.WriteLine("Grafo não lido!");
+                        break;
+                    }
+
+                    Console.WriteLine("Informe o vértice:");
+                    grafoLido.ImprimirArestasIncidentes(int.Parse(Console.ReadLine()));
+                    break;
+                case "6":
+                    if (grafoLido == null)
+                    {
+                        Console.WriteLine("Grafo não lido!");
+                        break;
+                    }
+
+                    Console.WriteLine("Informe a origem e o destino da aresta (ex: 0 1):");
+                    var entradaVertice = Console.ReadLine().Split(' ');
+                    grafoLido.ImprimirVerticesIncidentes(int.Parse(entradaVertice[0]), int.Parse(entradaVertice[1]));
+                    break;
+                case "7":
+                    if (grafoLido == null)
+                    {
+                        Console.WriteLine("Grafo não lido!");
+                        break;
+                    }
+
+                    Console.WriteLine("Informe o vértice:");
+                    grafoLido.ImprimirGrauVertice(int.Parse(Console.ReadLine()));
+                    break;
+                case "8":
+                    if (grafoLido == null)
+                    {
+                        Console.WriteLine("Grafo não lido!");
+                        break;
+                    }
+
+                    Console.WriteLine("Informe a origem, destino e o novo peso da aresta (ex: 0 1 10.5):");
+                    var dadosAresta = Console.ReadLine().Split(' ');
+                    int origem = int.Parse(dadosAresta[0]);
+                    int destino = int.Parse(dadosAresta[1]);
+                    double novoPeso = double.Parse(dadosAresta[2]);
+
+                    grafoLido.SubstituirPesoAresta(origem, destino, novoPeso);
+                    break;
+                case "15":
                     Console.WriteLine("Saindo da aplicação");
                     return;
                 default:
